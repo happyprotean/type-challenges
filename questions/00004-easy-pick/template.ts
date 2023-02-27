ec6af59507ca5003d4f345cb6fcc837f60c0193a
huanxi <2290991129@qq.com>
2023-02-28 00:49:02 +0800

    easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,7 @@
-type MyPick<T, K> = any
+// keyof 获取类型中的所有key的union
+// extends 类型限制
+// indexed access下标索引：通过类型对象取值的方式，获取一个类型里，某个key对应的类型
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
