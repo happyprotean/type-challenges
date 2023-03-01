236ce1f1cc3680939a85fb410e6896ba1eed18de
huanxi <2290991129@qq.com>
2023-03-01 23:47:34 +0800

    2023.3.1

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,12 @@
-type First<T extends any[]> = any
+// 直接判断是否为空数组
+// type First<T extends any[]> = T extends [] ? never : T[0]
+
+// 通过length判断，使用extend进行判断
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+
+// T[number]返回数组元素的union
+// 如果数组为空时，T[0]为undefined, T[number]为number，两者不匹配，返回never
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+
+// 数组解构，不用的数据使用_代替
+type First<T extends any[]> = T extends [infer First, ...infer _] ? First : never
--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,7 @@
-type Length<T> = any
+type Length<T extends readonly any[]> = T['length']
+
+// tuple是一种特殊的类型固定、长度固定的数组
+// tuple的length的类型是一个值，数组的length的类型是number
+type tu = [string, number]
+let val: tu = ['1', 1]
+
