43bc4f6a9cd22745f1dfc1271dbbc7f615a09620
huanxi <2290991129@qq.com>
2023-03-02 23:18:45 +0800

    2023.3.2 easy done

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,8 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<unknown> | { then: (onfulfilled: (arg: unknown) => any) => any }> = T extends Promise<infer P>
+  ? P extends Promise<unknown>
+    ? MyAwaited<P>
+    : P
+  : Thenable<T>
+
+type Thenable<T> =
+  T extends { then: (onfulfilled: (arg: infer X) => any) => any } ? X : never
--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils'
+
+// 如何遍历数组：使用...结构
+export type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
